7e02c562c8b25f0c22c0466e4cc96617d03c30c1
apple <apple@apples-MacBook-Pro.local>
2021-07-18 23:45:38 -0400

    updated lib

---
+++ tests/Employee.test.js
@@ -0,0 +1,10 @@
+const Employee = require("../lib/Employee");
+
+test("creates an employee objects", () => {
+    const employee = new Employee("Drake", 34, "greatest.ever@ovo.com ");
+
+    expect((employee.name))
+    
+
+
+});
