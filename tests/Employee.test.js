7d25d7ab03f37a3fe505062fe4f5c8f9d178fb57
Thetodd7 <85806673+Thetodd7@users.noreply.github.com>
2021-07-20 22:31:07 -0400

    Update Employee.test.js

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,10 +1,28 @@
 const Employee = require("../lib/Employee");
 
 test("creates an employee objects", () => {
-    const employee = new Employee("Drake", 34, "greatest.ever@ovo.com ");
+  const e = new Employee("Drake", 34, "greatest.ever@ovo.com ");
+  expect(typeof(e)).toBe("object")
 
-    expect((employee.name))
-    
+});
+test("gets employee name", () => {
+  const employee = new Employee("Drake", 34, "greatest.ever@ovo.com ");
+  expect(employee.getName()).toEqual(expect.any(String));
+});
+test("gets employee ID", () => {
+  const employee = new Employee("Drake", 34, "greatest.ever@ovo.com ");
 
+  expect(employee.getId()).toEqual(expect.any(Number));
+});
+test("gets employee email", () => {
+  const employee = new Employee("Drake", 34, "greatest.ever@ovo.com ");
 
+  expect(employee.getEmail()).toEqual(
+    expect.stringContaining(employee.email.toString())
+  );
+});
+test("gets role of employee", () => {
+  const employee = new Employee("Drake", 34, "greatest.ever@ovo.com ");
+
+  expect(employee.getRole()).toEqual("Employee");
 });
